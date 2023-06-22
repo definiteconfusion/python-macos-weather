8c483b8a1dcb3573ab4bae5f211d460bef5766a0
definiteconfusion <122401194+definiteconfusion@users.noreply.github.com>
2023-06-22 16:04:49 -0400

    Initial Commit

---
+++ inter/interApp.swift
@@ -0,0 +1,17 @@
+//
+//  interApp.swift
+//  inter
+//
+//  Created by Jake Rase on 6/22/23.
+//
+
+import SwiftUI
+
+@main
+struct interApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
